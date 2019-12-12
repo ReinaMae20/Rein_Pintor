abe665f4703cf80d25e3c801ec5df6dc4d3f969c
ReinaMae20 <58801557+ReinaMae20@users.noreply.github.com>
2019-12-12 14:53:22 +0800

    Add files via upload

---
+++ Exer1A.java
@@ -0,0 +1,14 @@
+public class Exer1A{
+	public  static void main (String[]args){
+		
+		int faveNumber = 20;
+		String faveCartChar = "SpongeBob";
+		char mi = 'V';
+		char nickNameArray [] = {'R','E','I','N'};
+		
+		System.out.println("FaveNumber"+"is my favorite Number ");
+		System.out.println("i love" + "Spongebob");
+		System.out.println("My Name is Reina Mae" + "mi"+ "Pintor");
+		System.out.println("You can call me" + nickNameArray[0]+ nickNameArray[1]+ nickNameArray[2]+ nickNameArray[3]);
+	}
+}
